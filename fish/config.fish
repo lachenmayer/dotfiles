3eff63b3456f771f699cea2991476bf314a48db4
Harry Lachenmayer <harrylachenmayer@gmail.com>
2016-09-12 16:20:34 +0100

    .

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set fish_path $HOME/.oh-my-fish
 set fish_custom $HOME/.dotfiles/fish
 
 set fish_theme lachenmayer
-set fish_plugins vi-mode node sublime z localhost
+set fish_plugins vi-mode
 
 set fish_greeting ""
 
@@ -17,7 +17,9 @@ set fish_greeting ""
 setenv GOPATH ~/.go
 
 # /usr/local/bin will be in the path twice, but that's probably better than messing around with /etc/paths.
-setenv PATH /usr/texbin ~/.cabal/bin /usr/local/bin $GOPATH/bin $PATH
+setenv PATH /usr/local/bin /usr/local/sbin $GOPATH/bin $PATH
 
 # Needed for cabal... Why isn't this set?
 setenv LIBRARY_PATH /usr/local/lib
+
+setenv HOMEBREW_GITHUB_API_TOKEN e38c8f6ebbb3c845a7114ac4f8808dbbb96757fb
