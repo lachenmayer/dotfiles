f0615cc8508d594a1d020f195ee486a999a81e3c
Harry Lachenmayer <harrylachenmayer@gmail.com>
2016-09-26 14:48:05 +0100

    add android stuff

--- fish/init.fish
+++ fish/init.fish
@@ -6,4 +6,8 @@ source $OMF_CONFIG/aliases.fish
 # /usr/local/bin will be in the path twice, but that's probably better than messing around with /etc/paths.
 setenv PATH /usr/local/bin /usr/local/sbin $PATH
 
+# Android shiz
+setenv ANDROID_HOME /usr/local/opt/android-sdk
+setenv PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools $PATH
+
 setenv HOMEBREW_GITHUB_API_TOKEN e38c8f6ebbb3c845a7114ac4f8808dbbb96757fb
