9c31dd1daf804e65435cdae9fc17e70532abcb6a
harry lachenmayer <harrylachenmayer@gmail.com>
2021-01-28 12:50:26 +0000

    add local.fish for local overrides

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,10 @@ switch (uname)
     source ~/.dotfiles/fish/linux.fish
 end
 
+if test -e ~/.dotfiles/fish/local.fish
+  source ~/.dotfiles/fish/local.fish
+end
+
 set -gx EDITOR "code -"
 setenv HOMEBREW_GITHUB_API_TOKEN e38c8f6ebbb3c845a7114ac4f8808dbbb96757fb
 set -gx LEDGER_FILE ~/Documents/finances/hledger.journal
