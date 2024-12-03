d8b8ac3568680e20d86a5b3e18b1269267eea3bb
harry lachenmayer <harrylachenmayer@gmail.com>
2024-12-03 12:13:52 +0000

    mac.fish: add homebrew env

--- fish/mac.fish
+++ fish/mac.fish
@@ -1,17 +1,20 @@
+# Add Homebrew to PATH
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Rust
 set -g fish_user_paths ~/.cargo/bin $fish_user_paths
 
 # Python
-set -g fish_user_paths ~/Library/Python/3.7/bin $fish_user_paths
+# set -g fish_user_paths ~/Library/Python/3.7/bin $fish_user_paths
 
 # Go
-set -gx GOPATH ~/.go
-set -g fish_user_paths $GOPATH/bin $fish_user_paths
+# set -gx GOPATH ~/.go
+# set -g fish_user_paths $GOPATH/bin $fish_user_paths
 
 # Android
-set -gx ANDROID_HOME $HOME/Library/Android/sdk
-set -g fish_user_paths $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $fish_user_paths
-set -gx ANDROID_NDK $ANDROID_HOME/ndk/21.4.7075529
+# set -gx ANDROID_HOME $HOME/Library/Android/sdk
+# set -g fish_user_paths $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $fish_user_paths
+# set -gx ANDROID_NDK $ANDROID_HOME/ndk/21.4.7075529
 
 # Ruby
 #set ruby_dir ruby@2.5
@@ -22,10 +25,10 @@ set -gx ANDROID_NDK $ANDROID_HOME/ndk/21.4.7075529
 #set -gx PKG_CONFIG_PATH "/usr/local/opt/$ruby_dir/lib/pkgconfig"
 
 # Homebrew Ruby
-set -g fish_user_paths /opt/homebrew/opt/ruby/bin /opt/homebrew/lib/ruby/gems/3.1.0/bin $fish_user_paths
-set -gx LDFLAGS "-L/opt/homebrew/opt/ruby/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/ruby/include"
-set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/ruby/lib/pkgconfig"
+# set -g fish_user_paths /opt/homebrew/opt/ruby/bin /opt/homebrew/lib/ruby/gems/3.1.0/bin $fish_user_paths
+# set -gx LDFLAGS "-L/opt/homebrew/opt/ruby/lib"
+# set -gx CPPFLAGS "-I/opt/homebrew/opt/ruby/include"
+# set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/ruby/lib/pkgconfig"
 
 # Disable Homebrew upgrade when installing packages
 set -gx HOMEBREW_NO_INSTALL_UPGRADE 1
